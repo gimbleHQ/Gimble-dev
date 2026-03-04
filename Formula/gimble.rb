4a19074f7dbe6fc3cd3b5e3edc6f0f1ba66ef683
Saket Pradhan <saketp@umich.edu>
2026-03-04 12:53:25 -0800

    v0.1.5 export

--- Formula/gimble.rb
+++ Formula/gimble.rb
@@ -1,15 +1,15 @@
 class Gimble < Formula
   desc "Gimble CLI"
   homepage "https://github.com/Saketspradhan/Gimble-dev"
-  version "0.1.4"
-  url "https://github.com/Saketspradhan/Gimble-dev/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "9bac0053877e79bb9d4f1e57e1ef17f41daf42a3ae35f1b5417a74248fa93b9e"
+  version "0.1.5"
+  url "https://github.com/Saketspradhan/Gimble-dev/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "890b877054b6dc09fb3b4619d80c815fca6a9bfb4f2e1165dec3f4c107439f8d"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-X main.version=0.1.4", "-o", bin/"gimble", "./cmd/gimble"
+    system "go", "build", "-ldflags", "-X main.version=0.1.5", "-o", bin/"gimble", "./cmd/gimble"
   end
 
   test do
