a002bd2246bcbe9eb2c27ef59f3bea55ade21d6e
Saket Pradhan <saketp@umich.edu>
2026-03-04 11:55:12 -0800

    Bump Homebrew formula to v0.1.4

--- Formula/gimble.rb
+++ Formula/gimble.rb
@@ -1,14 +1,15 @@
 class Gimble < Formula
   desc "Gimble CLI"
   homepage "https://github.com/Saketspradhan/Gimble-dev"
-  url "https://github.com/Saketspradhan/Gimble-dev/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "8262698522ad8a7dcf033ac815dc0c1f4132c0f3b1fae23ab4a8dbd1055a8da6"
+  version "0.1.4"
+  url "https://github.com/Saketspradhan/Gimble-dev/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "9bac0053877e79bb9d4f1e57e1ef17f41daf42a3ae35f1b5417a74248fa93b9e"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-X main.version=0.1.2", "-o", bin/"gimble", "./cmd/gimble"
+    system "go", "build", "-ldflags", "-X main.version=0.1.4", "-o", bin/"gimble", "./cmd/gimble"
   end
 
   test do
