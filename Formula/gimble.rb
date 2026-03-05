e9aefb42c862089489d3191ec9d3548810896fab
Saket Pradhan <saketp@umich.edu>
2026-03-05 11:57:12 -0800

    Fix Homebrew sha256 for v0.1.13 source tarball

--- Formula/gimble.rb
+++ Formula/gimble.rb
@@ -3,7 +3,7 @@ class Gimble < Formula
   homepage "https://github.com/Saketspradhan/Gimble-dev"
   version "0.1.13"
   url "https://github.com/Saketspradhan/Gimble-dev/archive/refs/tags/v0.1.13.tar.gz"
-  sha256 "6b8356a71a637d17321a86c64f58f44035fe66fcb709b02d63ec9c839925e7db"
+  sha256 "f879eead6a1e028a8d98808872fd77270dfe70e6e3282e8e6c6567280f2be3fe"
   license "MIT"
 
   depends_on "go" => :build
